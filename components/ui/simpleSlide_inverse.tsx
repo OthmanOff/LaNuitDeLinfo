2c7a513fdca312e1fea579d0a1e02509ca5fb131
Othmanbdg <othman92390@hotmail.fr>
2024-12-11 18:40:28 +0100

    fix

--- components/ui/simpleSlide_inverse.tsx
+++ components/ui/simpleSlide_inverse.tsx
@@ -7,27 +7,14 @@ interface SimpleSlideProps {
     h: number; // Hauteur de l'image
     alt:string;
     buttonText: string; // Texte du bouton
-    onButtonClick?: () => void; // Callback lorsque le bouton est cliqué
+
   }
   
-  const SimpleSlide: React.FC<SimpleSlideProps> = ({ img, text, w, h,alt , buttonText, onButtonClick}) => {
+  const SimpleSlide: React.FC<SimpleSlideProps> = ({ img, text, w, h,alt , buttonText}) => {
     return (
       <div className="flex justify-center items-center text-6xl bg-[#7de3ec] w-full">
         <span className="ml-4">{text}</span>
         <img src={img} alt={alt} width={w} height={h} />
-        <Button
-        onClick={handleClick} // Attacher le gestionnaire de clic
-        className="absolute bottom-4 left-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-      >
-        {buttonText} {/* Bouton à gauche */}
-      </Button>
-
-      <Button
-        onClick={handleClick} // Attacher le gestionnaire de clic
-        className="absolute bottom-4 right-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-      >
-        {buttonText} {/* Bouton à droite */}
-      </Button>
       </div>
     );
   };
